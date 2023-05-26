e60d16708208b704c9707a4d90281aecaac17185
srimathiradhakrishnan <srimathiradhakrishna@gmail.com>
2023-05-26 14:29:51 -0400

    updating keys and map

--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -1,11 +1,16 @@
 import React from "react";
 
+
 function ProjectItem({ name, about, technologies }) {
+   const techspan =technologies.map((technology)=>{return (<span key={technology}>{technology}</span>)})
+  
   return (
     <div className="project-item">
       <h3>{name}</h3>
       <p>{about}</p>
       <div className="technologies">
+     
+        {techspan}
         {/* render a <span> for each technology in the technologies array */}
       </div>
     </div>
--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -2,11 +2,11 @@ import React from "react";
 import ProjectItem from "./ProjectItem";
 
 function ProjectList({ projects }) {
-  console.log(projects);
+  const ProjectList =projects.map((project)=>{return (<ProjectItem key={project.id} {...project}/>)})
   return (
     <div id="projects">
       <h2>My Projects</h2>
-      <div id="project-list">{/* render ProjectItem components here */}</div>
+      <div id="project-list">{ProjectList}</div>
     </div>
   );
 }
